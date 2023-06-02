dc14f50debcf9d99b3e44cb8ef1d8d67d736b1ad
mr-exploit <raffiaj8@gmail.com>
2023-06-02 14:06:41 +0700

    penambahan user register, delete dan login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { get, GetAllUser } = require("./routes/routeuser");
+const { login, register } = require("./routes/routeauth");
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(get, GetAllUser);
+app.use(login, register);
 
 app.listen(port, '0.0.0.0', function() {
     console.log(`Your application is running on the port ${port}`);
