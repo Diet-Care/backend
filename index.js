ca186a4375e5ab787fe82e62508437e226c17454
muhamad raffi <65493711+mr-exploit@users.noreply.github.com>
2023-06-07 23:58:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const { getallcommentmakanan, getidcommentmakanan, createcommentmakanan, deletea
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3050;
 app.use(upload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
